cd7aa14f01f8095c4c98329b839328e8c799dfd1
Javier Moreno <javiomoreno@gmail.com>
2016-08-17 15:47:59 -0400

    creacion del recibo

--- views/recibo/view.php
+++ views/recibo/view.php
@@ -10,6 +10,11 @@ $this->title = "Recibo: ".$modelRecibo->ReciboNumero;
 $this->params['breadcrumbs'][] = ['label' => 'Recibos', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<style media="screen">
+  .recibo-view{
+    font-size: 10px;
+  }
+</style>
 <div class="recibo-view">
 
   <div class="row">
@@ -58,73 +63,108 @@ $this->params['breadcrumbs'][] = $this->title;
               </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-lg-3">
-              <?= $modelUsuario->UsuarioCedula;?>
-            </div>
-            <div class="col-lg-3">
-              <?= strtoupper($modelUsuario->UsuarioApellido).", ".strtoupper($modelUsuario->UsuarioNombre);?>
-            </div>
-            <div class="col-lg-3">
-              C. <?= $modelUsuario->UsuarioCedula;?>
-            </div>
-            <div class="col-lg-3">
-              Cargo <?= $modelUsuario->cargo->CargoNombre;?>
+          <div>
+            <div class="row" style="padding: 10px;">
+              <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" style="border: 1px solid #000;">
+                CÓDIGO
+              </div>
+              <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" style="border: 1px solid #000;">
+                DESCRIPCIÓN
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000;">
+                VALOR AUXILIAR
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000;">
+                ASIGNACIONES
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000;">
+                DEDUCCIONES
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000;">
+                NETO A COBRAR
+              </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-lg-3">
-              Fecha de Ingreso <?= $modelUsuario->UsuarioFechaIng;?>
-            </div>
-            <div class="col-lg-3">
-              Sueldo Diario: <?= $modelRecibo->ReciboSuelDiar;?>
-            </div>
-            <div class="col-lg-3">
-              Sueldo Semanal: <?= $modelRecibo->ReciboSuelSema;?>
-            </div>
-            <div class="col-lg-3">
-              Sueldo Mensual: <?= $modelRecibo->ReciboSuelMens;?>
+          <div>
+            <div class="row" style="margin-top: -10px; padding-right: 10px; padding-left: 10px;">
+              <div class="col-xs-1 col-sm-1 col-md-1 col-lg-1" style="border: 1px solid #000;">
+                <?php foreach ($modelParametro as $value): ?>
+                  <?= $value->ParametroCodigo;?>
+                <?php endforeach; ?>
+              </div>
+              <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3" style="border: 1px solid #000;">
+                <?php foreach ($modelParametro as $value): ?>
+                  <?= $value->ParametroCodigo;?>
+                  <br>
+                <?php endforeach; ?>
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000;">
+                <?php foreach ($modelParametro as $value): ?>
+                  <?= $value->ParametroValorAuxiliar;?>
+                  <br>
+                <?php endforeach; ?>
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000;">
+                <?php foreach ($modelParametro as $value): ?>
+                  <?php if (substr($value->ParametroCodigo, 0, 1) == "A" ): ?>
+                    <?= $value->ParametroValor;?>
+                  <?php else: ?>
+                    0
+                  <?php endif;?>
+                  <br>
+                <?php endforeach; ?>
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000;">
+                <?php foreach ($modelParametro as $value): ?>
+                  <?php if (substr($value->ParametroCodigo, 0, 1) == "R" ): ?>
+                    <?= $value->ParametroValor;?>
+                  <?php else: ?>
+                    0
+                  <?php endif;?>
+                  <br>
+                <?php endforeach; ?>
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000;">
+                <?php foreach ($modelParametro as $value): ?>
+                  <br>
+                <?php endforeach; ?>
+              </div>
             </div>
           </div>
-          <div class="row">
-            <div class="col-lg-3">
-              <strong>Número de <?= $modelRecibo->ReciboNumero;?></strong>
+          <div>
+            <div class="row" style="padding-left: 10px;">
+              <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+                <div class="row">
+                  <div class="col-lg-12" style=" padding: 10px; border: 1px solid #000;">
+                    <br>
+                    Recibi Conforme <br><br>
+                    _______________________________
+                  </div>
+                </div>
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000; margin-left: -4px;">
+                Total Trabajador
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000; margin-left: -2px;">
+                <?= $modelRecibo->ReciboAsignacion ?>
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000; margin-left: -2px;">
+                <?= (intval($modelRecibo->ReciboDeduccion) + intval($modelRecibo->ReciboRetencion)); ?>
+              </div>
+              <div class="col-xs-2 col-sm-2 col-md-2 col-lg-2" style="border: 1px solid #000; ">
+                <?= $modelRecibo->ReciboTotal ?>
+              </div>
             </div>
           </div>
-          <?php foreach ($modelParametro as $value): ?>
-            <div class="row">
-              <div class="col-lg-2">
-                <?= $modelRecibo->ReciboFecha;?>
+          <div>
+            <div class="row" style="padding-left: 10px;">
+              <div class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
               </div>
-              <div class="col-lg-2">
-                <?= $value->ParametroCodigo;?>
+              <div class="col-xs-8 col-sm-8 col-md-8 col-lg-8" style="border: 1px solid #000; margin-left: -4px; margin-top: -60px;">
+                <br>
+                Banco: <?= $modelUsuario->UsuarioBanco?><br><br>
+                CUENTA: <?= $modelUsuario->UsuarioCuentaBanco?>
               </div>
-              <div class="col-lg-2">
-                aqui falta
-              </div>
-              <div class="col-lg-2">
-                <?= $value->ParametroValorAuxiliar;?>
-              </div>
-              <div class="col-lg-2">
-                <?= $value->ParametroValor?>
-              </div>
-              <div class="col-lg-2">
-                Aqui Falta
-              </div>
-            </div>
-          <?php endforeach; ?>
-          <div class="row">
-            <div class="col-lg-3">
-              Asignación: <?= $modelRecibo->ReciboAsignacion;?>
-            </div>
-            <div class="col-lg-3">
-              Deducción: <?= $modelRecibo->ReciboDeduccion;?>
-            </div>
-            <div class="col-lg-3">
-              Retención: <?= $modelRecibo->ReciboRetencion;?>
-            </div>
-            <div class="col-lg-3">
-              Total:
             </div>
           </div>
         </div>
